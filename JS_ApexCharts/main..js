0ed94b7df865de28e6636c829e4094d93ce3d753
Phil <gentrs80@gmail.com>
2018-10-12 12:01:54 -0700

    add: series data and xaxis labels

--- JS_ApexCharts/main..js
+++ JS_ApexCharts/main..js
@@ -7,19 +7,45 @@ const options = {
     type: 'bar',
     background: '#f4f4f4',
     forecolor: '#333'
-  }
+  },
+
   // Series data has an array with an object inside
   series: [{
     name: 'Population',
-    data:[]
-  }]
-}
+     data: [
+      8550405,
+      3971883,
+      2720546,
+      2296224,
+      1567442,
+      1563025,
+      1469845,
+      1394928,
+      1300092,
+      1026908]
+  }],
+
+  xaxis: {
+    categories: [
+      "New York",
+      "Los Angeles",
+      "Chicago",
+      "Houston",
+      "Philadelphia",
+      "Phoenix",
+      "San Antonio",
+      "San Diego",
+      "Dallas",
+      "San Jose"
+    ]
+  }
+};
 
 
 // Init Chart 
 // Note you can get Element by ID
-const chart = new ApexChart(document.getElementById('chart'),options);
-// const chart = new ApexChart(document.querySelector('#chart'),options);
+// const chart = new ApexChart(document.getElementById('chart'),options);
+const chart = new ApexChart(document.querySelector("#chart"),options);
 
 // Render Chart
 chart.render();
