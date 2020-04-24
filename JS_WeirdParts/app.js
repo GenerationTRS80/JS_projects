c9178c9ea4e988f717ed72145f0e1b0b9107a2fc
Phil <gentrs80@gmail.com>
2020-04-24 13:52:17 -0700

    update: Execution Context

--- JS_WeirdParts/app.js
+++ JS_WeirdParts/app.js
@@ -1,12 +1,29 @@
-/* Execution Context -Create and hoisting */
+/* Global Execution Context 
+  1) Phase 1 - Create memory for the variables
+  2) Each function invokes a new "Execution Context"
 
+  -Create and hoisting
+
+*/
+
+function b() {
+  console.log("called b!");
+}
+
+b();
+
+console.log(a);
 
 var a = "Hello World";
 
-function b() {
-  console.log("Called B!");
-}
-
-
-b();
 console.log(a);
+
+// undefined is the value that variables receive in the creation phase
+// of creating an execution context
+
+/* if (a === undefined) {
+
+  console.log('a is Undefined!');
+} else {
+  console.log('a is defined');
+} */
