acb0fd5bcd8113dcfcf9abcc69be692ab2bdc004
Phil <gentrs80@gmail.com>
2020-06-12 13:39:52 -0700

    init: Existence and Booleans

---
+++ JS_WeirdParts/Booleans.js
@@ -0,0 +1,14 @@
+//  Existence and booleans
+
+var a;
+
+// goes out to the internet and looks for a value
+
+//a = null;
+// a = "";
+// a = "hello world";
+a = 0;
+
+if (a || a === 0) {
+  console.log('Something is there.');
+}
