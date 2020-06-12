7992779f6c27b342a5ccadb28f2937ba18779ed0
Phil <gentrs80@gmail.com>
2020-06-12 13:41:07 -0700

    init: Operator

---
+++ JS_WeirdParts/Operator.js
@@ -0,0 +1,27 @@
+// Operator
+
+var a = (3 + 4) * 5;
+
+var a = 2,
+  b = 3,
+  c = 4;
+
+a = b = c;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+//Type Coercion
+
+var a = 'Hello' + ' ' + 'World';
+var a = 1 + '2';
+console.log(a);
+
+
+//Comparison Operators
+console.log(3 < 2);
+console.log(false < 1);
+
+// This statement is TRUE
+console.log(3 < 2 < 1);
