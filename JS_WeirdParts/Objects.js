c500813aa190d33f3dbe5e0bd3d8ad0f3a10eb8d
Phil <gentrs80@gmail.com>
2020-06-12 17:14:43 -0700

    init: JavaScript Objects

---
+++ JS_WeirdParts/Objects.js
@@ -0,0 +1,30 @@
+// Objects are collections of name value pairs
+// Objects can have properties and methods
+// Methods are functions
+
+var person = new Object();
+//var person = {};
+
+// Add properties
+person["firstname"] = "Tony";
+person["lastname"] = "Alicia";
+
+
+var firstNameProperty = "firstname";
+
+console.log(person);
+console.log(person[firstNameProperty]);
+
+// Use Dot operator
+console.log('Dot operator ' + person.firstname);
+
+// Add sub object
+person.address = new Object();
+
+person.address.street = "111 Main St.";
+person.address.city = "New York";
+person.address.state = "NY";
+
+console.log(person.address.street);
+console.log(person.address.city);
+console.log(person["address"]["state"]);
