2e85fab8c499fab3dc750969a7b4cd2ba9234023
Phil <gentrs80@gmail.com>
2020-06-16 17:44:01 -0700

    update: June 16 2020

--- JS_WeirdParts/FunctionConstructors.js
+++ JS_WeirdParts/FunctionConstructors.js
@@ -1,13 +1,16 @@
 // Function Constructors - A normal function that is used to construct objects
 
 
-// A Call a function used specifically to create on object. Is called a constructor
+// A function used specifically to create on object. Is called a "function constructor"
+
+//NOTE: When the execution cont
 
 function Person(firstname, lastname) {
 
-  // Note: This keyword is point  to an empty object in memory
+  // Note: This keyword is point to an empty object in memory
   console.log(this);
 
+  // this variable points to the empty object that is invoked my the keyword new
   this.firstname = firstname;
   this.lastname = lastname;
   console.log('This function is invoked!');
@@ -16,6 +19,7 @@ function Person(firstname, lastname) {
 // Use the "new" key word operator
 
 // The new keyword is allow the construction of an object via a function
+// "new" keyword action: creates and empty object then invokes/calls the function Person
 var john = new Person('John', 'Doe');
 console.log(john);
 
@@ -26,4 +30,4 @@ var jane = new Person(
   'Doe'
 );
 
-console.log(jane);
+// console.log(jane);
