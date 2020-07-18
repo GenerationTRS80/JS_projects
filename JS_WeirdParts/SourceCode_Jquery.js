709fdab793f014a76ab57aebd353cc1ae5a2497e
Phil <gentrs80@gmail.com>
2020-07-17 17:24:32 -0700

    init: jquery example

---
+++ JS_WeirdParts/SourceCode_Jquery.js
@@ -0,0 +1,10 @@
+// Looking at the J Query library
+
+
+// search all ul that have a class people and find each child
+// var q = $("ul.people").addClass("NewClass").removeClass("People");
+
+var q = $("ul.people li");
+
+
+console.log(q);
